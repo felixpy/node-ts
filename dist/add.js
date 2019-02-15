53b9d78f7e9df01697197f5c0dde857a24dd60e7
felixpy <felixpy.1993@gmail.com>
2019-02-15 12:37:28 +0800

    feat: add basic files

---
+++ dist/add.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwoNumber(x = 0, y = 0) {
+    return x + y;
+}
+exports.default = addTwoNumber;
+//# sourceMappingURL=add.js.map
